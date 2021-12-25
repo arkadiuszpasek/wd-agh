3a182a046c5f0791bb5b3b73ac9e73b05c80f10f
Arek Pasek <arkadiusz.pasek@closer.app>
2021-12-25 09:48:12 +0100

    husky

--- src/organisms/LoginDialog/LoginDialog.tsx
+++ src/organisms/LoginDialog/LoginDialog.tsx
@@ -1,20 +1,24 @@
-import {
-  Button, Link, Stack, Typography
-} from "@mui/material";
+import { Button, Link, Stack, Typography } from "@mui/material";
 import React from "react";
 import { L10n } from "../../models/intl/L10n/L10n";
 import { LoginInputs } from "../../molecules/LoginInputs/LoginInputs";
 import { theme } from "../../styles/palette";
 
 export const LoginDialog = () => {
-  console.log("themeprimar", theme.palette.primary)
+  console.log("themeprimar", theme.palette.primary);
   return (
     <Stack spacing={2}>
       <Typography variant="h5">
         <L10n id="login" />
       </Typography>
       <LoginInputs />
-      <Button variant="contained" style={{ backgroundColor: theme.palette.primary.main, color: theme.palette.primary.contrastText }}>
+      <Button
+        variant="contained"
+        style={{
+          backgroundColor: theme.palette.primary.main,
+          color: theme.palette.primary.contrastText,
+        }}
+      >
         <L10n id="login.logIn" />
       </Button>
       <Link
